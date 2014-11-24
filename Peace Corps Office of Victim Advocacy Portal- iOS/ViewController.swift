e88345eeda271a6106f793a505f9362f9b910b73
bellaincanto <shelagh_mcgowan@my.uri.edu>
2014-11-24 16:48:02 -0500

    Initial Commit

---
+++ Peace Corps Office of Victim Advocacy Portal- iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peace Corps Office of Victim Advocacy Portal- iOS
+//
+//  Created by Shelagh McGowan on 11/24/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Peace Corps Office of Victim Advocacy Portal- iOSTests/Peace_Corps_Office_of_Victim_Advocacy_Portal__iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Peace_Corps_Office_of_Victim_Advocacy_Portal__iOSTests.swift
+//  Peace Corps Office of Victim Advocacy Portal- iOSTests
+//
+//  Created by Shelagh McGowan on 11/24/14.
+//  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Peace_Corps_Office_of_Victim_Advocacy_Portal__iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
